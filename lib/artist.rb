ba8e51e050276e11607bffe4794bbfe092662f7f
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 07:34:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   extend Memorable::ClassMethods
-  include Memorable::InstanceMethod
+  include Memorable::InstanceMethods
 
   @@artists = []
 
