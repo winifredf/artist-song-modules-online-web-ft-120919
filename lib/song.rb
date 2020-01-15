189e729dae5ee4b7e47ef7580fa509b055883578
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 07:31:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,6 @@ require_relative '../lib/concerns/memorable'
 class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
   attr_accessor :name
   attr_reader :artist
 
